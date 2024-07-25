882cfc1f4d6f065df10a7c27129f647be3b6f107
has673 <hassansajjas>
2024-07-25 13:50:30 +0500

    initial

---
+++ src/app/components/Model.jsx
@@ -0,0 +1,34 @@
+import { useAnimations, useGLTF, ScrollControls, useScroll } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+
+useGLTF.preload('./robot_playground.glb')
+
+const Model = () => {
+  const ref = useRef()
+  const { nodes, materials, animations, scene } = useGLTF('./robot_playground.glb')
+  const { actions } = useAnimations(animations, scene)
+  const scroll = useScroll()
+
+  useEffect(() => {
+    actions['Experiment'].play().paused = true
+  }, [])
+
+  useFrame(
+    () =>
+      //@ts-ignore
+      (actions["Experiment"].time =
+        //@ts-ignore
+        (actions["Experiment"].getClip().duration * scroll.offset) / 4)
+  )
+
+  return (
+    <group ref={ref} scale={[1.5, 1.5, 1.5]}>
+      <primitive object={scene} />
+    </group>
+  )
+}
+
+
+
+export default Model
