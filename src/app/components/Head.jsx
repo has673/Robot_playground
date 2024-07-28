1a4c47f1cd0798df15af4e2e095d2d90bbabbf14
has673 <hassansajjas>
2024-07-28 23:27:23 +0500

    txt

---
+++ src/app/components/Head.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Html } from '@react-three/drei';
+
+
+const Head = () => {
+  return (
+    <Html position={[0, -3.3, 0]} center>
+      <h1  className="smoke-text">Robot </h1>
+    </Html>
+  );
+}
+
+export default Head;
--- src/app/components/Scene.js
+++ src/app/components/Scene.js
@@ -4,6 +4,7 @@ import { Canvas } from "@react-three/fiber";
 import Model from "./Model";
 import { Suspense } from "react";
 import { useProgress, Html, ScrollControls } from "@react-three/drei";
+import Head from "./Head";
 
 function Loader() {
   const { progress } = useProgress();
@@ -13,18 +14,22 @@ function Loader() {
 
 export default function Scene() {
   return (
+    <>
+    
     <Canvas 
       className="w-full h-full" 
       
     >
-   
     <color attach="background" args={['#cecbd1']} />
       <ambientLight position={[-5, -5, 5]} intensity={10} />
       <Suspense fallback={<Loader />}>
         <ScrollControls damping={1} pages={10}>
+        <Head/>
           <Model />
         </ScrollControls>
       </Suspense>
     </Canvas>
+    </>
+
   );
 }
